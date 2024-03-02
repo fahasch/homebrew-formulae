c3ad84a02117a67895d60dc21837e253aec4538b
fahasch <fahasch@yahoo.de>
2024-03-02 08:39:38 +0100

    try to fix hsbcl

--- Formula/hsbcl.rb
+++ Formula/hsbcl.rb
@@ -2,53 +2,21 @@ class Hsbcl < Formula
   desc "Hunchentoot Steel Bank Common Lisp system"
   homepage "http://www.sbcl.org/"
   url "https://sourceforge.net/projects/fricas/files/fricas/1.3.9/hsbcl-1.3.9.tar"
-  sha256 "da4f9486fad413d3af7abb1b09269325fdb4bff7e7b88f2195ddee743263cf7e"
+  sha256 "dc69541993ae3b14e1912e3160ba7a1e0f044be4af6ea46caa98dc6d65b54b6a"
   license all_of: [:public_domain, "MIT", "Xerox", "BSD-3-Clause"]
   
   depends_on "sbcl" => :build
 
   def install
-    # Remove non-ASCII values from environment as they cause build failures
-    # More information: https://bugs.gentoo.org/show_bug.cgi?id=174702
-    ENV.delete_if do |_, value|
-      ascii_val = value.dup
-      ascii_val.force_encoding("ASCII-8BIT") if ascii_val.respond_to? :force_encoding
-      ascii_val =~ /[\x80-\xff]/n
-    end
-
-    xc_cmdline = "ecl --norc"
-
-    args = [
-      "--prefix=#{prefix}",
-      "--xc-host=#{xc_cmdline}",
-      "--with-sb-core-compression",
-      "--with-sb-ldb",
-      "--with-sb-thread",
-    ]
-
-    ENV["SBCL_MACOSX_VERSION_MIN"] = MacOS.version.to_s if OS.mac?
-    system "./make.sh", *args
-
-    ENV["INSTALL_ROOT"] = prefix
-    system "sh", "install.sh"
-
-    # Install sources
-    bin.env_script_all_files libexec/"bin",
-                             SBCL_SOURCE_ROOT: pkgshare/"src",
-                             SBCL_HOME:        lib/"sbcl"
-    pkgshare.install %w[contrib src]
-    (lib/"sbcl/sbclrc").write <<~EOS
-      (setf (logical-pathname-translations "SYS")
-        '(("SYS:SRC;**;*.*.*" #p"#{pkgshare}/src/**/*.*")
-          ("SYS:CONTRIB;**;*.*.*" #p"#{pkgshare}/contrib/**/*.*")))
-    EOS
+    system "./build_hsbcl"
+    bin.install("hsbcl")
   end
 
   test do
     (testpath/"simple.sbcl").write <<~EOS
       (write-line (write-to-string (+ 2 2)))
     EOS
-    output = shell_output("#{bin}/sbcl --script #{testpath}/simple.sbcl")
+    output = shell_output("#{bin}/hsbcl --script #{testpath}/simple.sbcl")
     assert_equal "4", output.strip
   end
 end
